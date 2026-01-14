7c07d3ba0c780cb6acc7fea26bfd64fd70150ebf
Вероника Агафонова <uksus1488@bk.ru>
2026-01-14 12:59:03 +0300

    созданы страницы вместо окон

---
+++ Desktop/LoginPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Desktop
+{
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Переход на страницу регистрации (создай её аналогично)
+            // NavigationService.Navigate(new RegistrationPage());
+            MessageBox.Show("Переход на регистрацию");
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Здесь будет логика входа
+            MessageBox.Show("Логика входа");
+        }
+    }
+}
---
+++ Desktop/RegistrationPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Desktop
+{
+    public partial class RegistrationPage : Page
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        // Кнопка "Зарегистрироваться"
+        private void DoRegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Сюда потом добавишь сохранение в репозиторий
+            MessageBox.Show("Регистрация успешна!");
+            NavigationService.Navigate(new LoginPage());
+        }
+
+        // Кнопка "Назад" к логину
+        private void BackToLogin_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new LoginPage());
+        }
+    }
+}
